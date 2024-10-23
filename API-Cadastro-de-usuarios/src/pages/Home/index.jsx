c985b6a7a59a2cfb792ce9c05508383361c74491
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-22 21:37:18 -0400

    Construindo os campos do nosso HTML

--- API-Cadastro-de-usuarios/src/pages/Home/index.jsx
+++ API-Cadastro-de-usuarios/src/pages/Home/index.jsx
@@ -1,13 +1,58 @@
 import './style.css'
+import Trash from '../../assets/trash.svg'
+
 
 function Home() {
 
+  const users = [{
+    id: '12345657jsj',
+    name: 'Vinicius',
+    age: 18,
+    email: 'Vinicius.solique@ganso.com.br'
+  }, {
+
+    id: '123456578vsj',
+    name: 'Ingrid',
+    age: 28,
+    email: 'ingrid.solique@gmail.com'
+  }
+  ]
+
   return (
-  
-      <div>
-        <h1>Olá React</h1>
-      </div>
-      
+
+    <div className='container'>
+
+      <form>
+        <h1>Cadastro de Estudantes</h1>
+        <input name='Nome' type='text' />
+        <input name='Idade' type='number' />
+        <input name='E-mail' type='email' />
+        <button type='buttton'>Cadastrar Estudantes</button>
+      </form>
+
+      {
+        users.map(user => (
+
+          <div key={user.id}>
+            <div>
+              <p>Nome: {user.name} </p>
+              <p>Idade: {user.age}</p>
+              <p>Email: {user.email} </p>
+            </div>
+            <button>
+              <img src={Trash} />
+            </button>
+          </div>
+
+
+        ))
+      }
+
+
+
+
+    </div>
+
   )
 }
 
