a9a3f5af783ea8c0551094aaa0ffdfc702095b4e
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-23 21:28:20 -0400

    Um esboço do formulário

--- API-Cadastro-de-usuarios/src/pages/Login/tela.js
+++ API-Cadastro-de-usuarios/src/pages/Login/tela.js
@@ -6,13 +6,27 @@ function Tela() {
 
     return (
 
-        <div className='container'>
-
-            <form>
-
-            </form>
+        <form>
+        <div>
+          <label htmlFor="usuario">Usuário:</label>
+          <input type="text" id="usuario" name="usuario" required />
 
         </div>
+  
+        <div>
+          <label htmlFor="senha">Senha:</label>
+          <input type="password" id="senha" name="senha"required/>
+        </div>
+  
+        <div>
+          <label htmlFor="lembrar">
+            <input type="checkbox" id="lembrar" name="lembrar"/>
+            Lembre-me
+          </label>
+        </div>
+  
+        <button type="submit">Entrar</button>
+      </form>
     )
 
 }
