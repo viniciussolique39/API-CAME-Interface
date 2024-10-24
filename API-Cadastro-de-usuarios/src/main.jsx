0d974e545c78bae4785c2829b3aea2c0d141d10a
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-23 21:17:07 -0400

    Importando a página de login do sistema

--- API-Cadastro-de-usuarios/src/main.jsx
+++ API-Cadastro-de-usuarios/src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Home from './pages/Home'
+import Login from './pages/Login'
 import './index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Home />
+    <Home/>
+    <Login/>
   </StrictMode>,
 )
