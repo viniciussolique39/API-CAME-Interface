744be08d3bbb00a3cb8765ccc1b47c98f70b743e
Vinicius Solique <viniciussolique39@gmail.com>
2024-10-23 21:19:22 -0400

    Criando o formulário de login do sistema

--- API-Cadastro-de-usuarios/src/pages/Login/tela.js
+++ API-Cadastro-de-usuarios/src/pages/Login/tela.js
@@ -1,12 +1,20 @@
-import {} from 'react'
+import { } from 'react'
 
 
-function Tela ()
-
-return(
-
-)
+function Tela() {
 
 
+    return (
+
+        <div className='container'>
+
+            <form>
+
+            </form>
+
+        </div>
+    )
+
+}
 
 export default Tela 
